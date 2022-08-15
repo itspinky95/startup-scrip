03fd533035f77dc205b0418d46d1ef1710d626c1
itspinky95 <itspinky95@hotmail.com>
2022-08-15 21:43:03 +0100

    writing main basic script

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y 
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install apache2 -y
+echo "Apache2 Done"
+sudo apt install default-jdk -y 
+sudo apt install default-jre -y 
+echo "Java Done"
+sudo apt install c++ -y
+sudo apt install build-essential -y 
+echo "C++ Done"
